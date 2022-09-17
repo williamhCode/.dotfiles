32d682d09378ff76c7013ffc64a910bf3255a762
williamhCode <83525937+williamhCode@users.noreply.github.com>
2022-09-17 03:03:23 -0400

    sessionizer bug fix

--- bin/.local/scripts/tmux-sessionizer.sh
+++ bin/.local/scripts/tmux-sessionizer.sh
@@ -20,13 +20,13 @@ selected_name=$(basename "$selected" | tr . _)
 
 # if tmux is not attached, create session/ attach
 if [[ -z $TMUX ]]; then
-    tmux new-session -A -s $selected_name -c $selected
+    tmux new-session -A -s "$selected_name" -c "$selected"
 
 # if tmux is attached and session doesn't exist, create session
 elif ! tmux has-session -t=$selected_name 2> /dev/null; then
-    tmux new-session -ds $selected_name -c $selected
+    tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
 # if tmux is attached, switch to session
-tmux switch-client -t $selected_name
+tmux switch-client -t "$selected_name"
 
